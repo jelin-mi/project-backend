8957b1ab812ae835b3b1a2d2687bf3f2d6059b1f
“jelin.mi” <jelin.mi@gmail.com>
2022-02-26 14:49:24 +0100

    update

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-require("dotenv/config");
-require("./db");
-const express = require("express");
+require('dotenv/config');
+require('./db');
+const express = require('express');
 
-
-const { isAuthenticated } = require("./middleware/jwt.middleware");
-const allRoutes = require("./routes");
-const authRouter = require("./routes/auth.routes");
-const protectedRoute = require("./routes/protected.routes");
+const { isAuthenticated } = require('./middleware/jwt.middleware');
+const allRoutes = require('./routes');
+const authRouter = require('./routes/auth.routes');
+const protectedRoute = require('./routes/protected.routes');
 
 const app = express();
 
-require("./config")(app);
+require('./config')(app);
 
-app.use("/api", allRoutes);
-app.use("/api/protected", isAuthenticated, protectedRoute);
-app.use("/auth", authRouter);
+app.use('/api', allRoutes);
+app.use('/api/protected', isAuthenticated, protectedRoute);
+app.use('/auth', authRouter);
 
-require("./error-handling")(app);
+require('./error-handling')(app);
 
 module.exports = app;
