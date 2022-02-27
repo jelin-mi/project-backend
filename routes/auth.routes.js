8cb282d77c99ece89e09f0dffc75c3341334c7f6
“jelin.mi” <jelin.mi@gmail.com>
2022-02-27 22:25:47 +0100

    commented parts N/A for my project

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,12 +7,12 @@ const { isAuthenticated } = require('../middleware/jwt.middleware');
 const router = express.Router();
 const saltRounds = 10;
 
-// POST  /auth/signup
+//---------- POST  /auth/signup ----------
 router.post('/signup', (req, res, next) => {
-  const { email, password, name } = req.body;
+  const { email, password/* , name */ } = req.body;
 
   // Check if email or password or name are provided as empty string
-  if (email === '' || password === '' || name === '') {
+  if (email === '' || password === '' /* || name === '' */) {
     res.status(400).json({ message: 'Provide email, password and name' });
     return;
   }
@@ -48,15 +48,15 @@ router.post('/signup', (req, res, next) => {
 
       // Create the new user in the database
       // We return a pending promise, which allows us to chain another `then`
-      return User.create({ email, password: hashedPassword, name });
+      return User.create({ email, password: hashedPassword/* , name */ });
     })
     .then(createdUser => {
       // Deconstruct the newly created user object to omit the password
       // We should never expose passwords publicly
-      const { email, name, _id } = createdUser;
+      const { email/* , name */, _id } = createdUser;
 
       // Create a new object that doesn't expose the password
-      const user = { email, name, _id };
+      const user = { email/* , name */, _id };
 
       // Send a json response containing the user object
       res.status(201).json({ user: user });
@@ -67,7 +67,7 @@ router.post('/signup', (req, res, next) => {
     });
 });
 
-// POST  /auth/login
+//---------- POST  /auth/login ----------
 router.post('/login', (req, res, next) => {
   const { email, password } = req.body;
 
@@ -91,10 +91,10 @@ router.post('/login', (req, res, next) => {
 
       if (passwordCorrect) {
         // Deconstruct the user object to omit the password
-        const { _id, email, name } = foundUser;
+        const { _id, email/* , name */ } = foundUser;
 
         // Create an object that will be set as the token payload
-        const payload = { _id, email, name };
+        const payload = { _id, email/* , name */ };
 
         // Create and sign the token
         const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
@@ -111,7 +111,7 @@ router.post('/login', (req, res, next) => {
     .catch(err => res.status(500).json({ message: 'Internal Server Error' }));
 });
 
-// GET  /auth/verify
+//---------- GET  /auth/verify ----------
 router.get('/verify', isAuthenticated, (req, res, next) => {
   // If JWT token is valid the payload gets decoded by the
   // isAuthenticated middleware and made available on `req.payload`
