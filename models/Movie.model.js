d1671d6b4438b10d3623d9ac1805ab043d99c31d
“jelin.mi” <jelin.mi@gmail.com>
2022-02-27 18:42:54 +0100

    User and Movie models created

---
+++ models/Movie.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const movieSchema = new Schema({
+  owner: { type: Schema.Types.ObjectId, ref: 'User' },
+  name: {
+    type: String,
+    required: true,
+  },
+  year: Number,
+  director: String,
+  channel: String,
+  buddy: String,
+  synopsis: String,
+  rating: { type: Number, enum: [1, 2, 3] },
+});
+
+module.exports = model('Movie', movieSchema);
--- models/User.model.js
+++ models/User.model.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true, match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'] },
+  hashedPassword: { type: String, required: [true, 'password is required'] },
+  userName: String,
+  favouriteMovies: String,
+  preferredDirector: String,
+  myBuddies: String,
+  avatar: String,
 });
 
 module.exports = model('User', userSchema);
