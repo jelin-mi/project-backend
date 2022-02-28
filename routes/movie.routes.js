7550f87f1474104b6e773e2e77f3fe2bf20f5fe5
“jelin.mi” <jelin.mi@gmail.com>
2022-02-28 19:56:39 +0100

    movie routes updated

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,77 +1,87 @@
-// mycode:
-const express = require('express');
+const router = require('express').Router();
+const mongoose = require('mongoose');
+
 const Movie = require('../models/Movie.model');
 
-function filmRoutes() {
-  const router = express.Router();
+// CREATE ----------> POSTMAN: POST http://localhost: 5005/api/movies (envío el objeto de la L9)
+router.post('/movies', async (req, res, next) => {
+  const { name, year, director, channel, buddy, synopsis, rating } = req.body;
 
-  // CREATE ----------> POSTMAN: POST http://localhost: 5005/films (envío el objeto de la L10)
-  router.post('/', async (req, res, next) => {
-    const { name, year, director, channel, buddy, synopsis, rating } = req.body;
+  try {
+    const movie = await Movie.create({ name, year, director, channel, buddy, synopsis, rating });
+    res.json({
+      created: movie,
+    });
+  } catch (e) {
+    next(e);
+  }
+});
 
-    try {
-      const movie = await Movie.create({ name, year, director, channel, buddy, synopsis, rating });
+// READ all ----------> POSTMAN: GET http://localhost: 5005/api/movies
+router.get('/movies', async (req, res, next) => {
+  try {
+    const movieResponse = await Movie.find();
+    res.json(movieResponse);
+  } catch (e) {
+    next(e);
+  }
+});
 
-      res.json({
-        created: movie,
-      });
-    } catch (e) {
-      next(e);
+// READ detail ----------> POSTMAN: GET http://localhost: 5005/api/movies/<some id>
+router.get('/movies/:id', async (req, res, next) => {
+  const { id } = req.params;
+
+  // Check if the id string provided through the URL parameter is a valid Hexadecimal string.
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  try {
+    const movie = await Movie.findById(id);
+
+    if (movie === null) {
+      return res.status(404).json({ error: 'Movie not found' });
     }
-  });
+    return res.status(200).json(movie);
+  } catch (e) {
+    next(e);
+  }
+});
 
-  // READ all ----------> POSTMAN: GET http://localhost: 5005/films
-  router.get('/', async (req, res, next) => {
-    try {
-      const movieResponse = await Movie.find();
-      res.json(movieResponse);
-    } catch (e) {dd
-      next(e);
-    }
-  });
+// UPDATE ----------> POSTMAN: PUT http://localhost: 5005/api/movies/<some id> (envío el objeto de la L10)
+router.put('/movies/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const { name, year, director, channel, buddy, synopsis, rating } = req.body;
 
-  // READ detail ----------> POSTMAN: GET http://localhost: 5005/films/<some id>
-  router.get('/:id', async (req, res, next) => {
-    const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
 
-    try {
-      const movie = await Movie.findById(id);
+  try {
+    const movie = await Movie.findByIdAndUpdate(id, { name, year, director, channel, buddy, synopsis, rating }, { new: true });
+    res.json(movie);
+  } catch (e) {
+    next(e);
+  }
+});
 
-      if (movie ===  null) {
-        return res.status(404).json({ error: 'Movie not found' });
-      }
-      return res.json(movie);
-    } catch (e) {
-      next(e);
-    }
-  });
+// DELETE ----------> POSTMAN: DELETE http://localhost: 5005/api/movies/<some id>
+router.delete('/movies/:id', async (req, res, next) => {
+  const { id } = req.params;
 
-  // UPDATE ----------> POSTMAN: PUT http://localhost: 5005/films/<some id> (envío el objeto de la L10)
-  router.put('/:id', async (req, res, next) => {
-    const { id } = req.params;
-    const { name, year, director, channel, buddy, synopsis, rating } = req.body;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
 
-    try {
-      const movie = await Movie.findByIdAndUpdate(id, { name, year, director, channel, buddy, synopsis, rating }, { new: true });
-      res.json(movie);
-    } catch (e) {
-      next(e);
-    }
-  });
+  try {
+    const movie = await Movie.findByIdAndDelete(id);
+    res.json(movie, { message: `Movie with ${id} is removed successfully.` });
+  } catch (e) {
+    next(e);
+  }
+});
 
-  // DELETE ----------> POSTMAN: DELETE http://localhost: 5005/films/<some id>
-  router.delete('/:id', async (req, res, next) => {
-    const { id } = req.params;
-
-    try {
-      const movie = await Movie.findByIdAndDelete(id);
-      res.json(movie);
-    } catch (e) {
-      next(e);
-    }
-  });
-
-  return router;
-}
-
-module.exports = filmRoutes;
+module.exports = router;
