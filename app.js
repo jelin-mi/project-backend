066043399b0f5095bd40689488529cbbd25d3201
“jelin.mi” <jelin.mi@gmail.com>
2022-02-28 19:56:07 +0100

    clean the code

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const { isAuthenticated } = require('./middleware/jwt.middleware');
 const allRoutes = require('./routes');
 const authRouter = require('./routes/auth.routes');
 const protectedRoute = require('./routes/protected.routes');
-// mycode:
-const filmRoutes = require('./routes/movie');
+const movieRouter = require('./routes/movie.routes');
 
 const app = express();
 
@@ -15,10 +14,8 @@ require('./config')(app);
 
 app.use('/api', allRoutes);
 app.use('/api/protected', isAuthenticated, protectedRoute);
-app.use('/auth', authRouter);     // authRouter para las rutas del modelo User
-
-// mycode:
-app.use('/films', filmRoutes); // filmRoutes para las rutas del modelo Movie
+app.use('/auth', authRouter);
+app.use('/api', movieRouter);
 
 require('./error-handling')(app);
 
