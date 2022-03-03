390718d5025f8ca45026da36754e37f346ec474b
“jelin.mi” <jelin.mi@gmail.com>
2022-03-03 19:45:42 +0100

    N/A removed

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ require('dotenv/config');
 require('./db');
 const express = require('express');
 const { isAuthenticated } = require('./middleware/jwt.middleware');
-
-/* const allRoutes = require('./routes'); */
 const authRouter = require('./routes/auth.routes');
-/* const protectedRoute = require('./routes/protected.routes'); */
 const movieRouter = require('./routes/movie.routes');
 const userRouter = require('./routes/user.routes');
 const watchlistRouter = require('./routes/watchlist.routes');
@@ -13,8 +10,6 @@ const watchlistRouter = require('./routes/watchlist.routes');
 const app = express();
 require('./config')(app);
 
-/* app.use('/api', allRoutes); */
-/* app.use('/api/protected', isAuthenticated, protectedRoute); */
 app.use('/auth', authRouter);
 app.use('/api/movies', movieRouter);
 app.use('/api/watchlist', watchlistRouter);
